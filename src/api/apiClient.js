18f06eec3e5726f80b29b7f27248e502aebbaacf
IMAMteach09 <mi.bakelli@esi-sba.dz>
2026-06-02 13:11:14 +0100

    modified again

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -8,7 +8,8 @@ export const clearToken = () => localStorage.removeItem('noteMeToken');
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, 
+  
+  baseURL: 'https://noteme-backend-my0y.onrender.com', 
   withCredentials: true,
 });
 
@@ -36,3 +37,4 @@ export async function apiFetch(path, options = {}) {
 
   return data;
 }
+export default apiClient;
